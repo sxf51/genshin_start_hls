bb60c44e13955b891b5166c542b885fa7ca3189d
Puze <puzel@student.unimelb.edu.au>
2026-09-06 11:06:55 +1000

    Q2_1

--- _ide/workspace_journal_65526.py
+++ _ide/workspace_journal_65526.py
@@ -1,6 +1,8 @@
-# 2026-09-06T11:06:41.040513512
+# 2026-09-06T11:06:12.977466687
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="genshin_start_hls")
+
+vitis.dispose()
 
--- fir128/fir.h
+++ fir128/fir.h
@@ -11,12 +11,12 @@
 
 const int N=128;
 
-// typedef ap_int<16>	coef_t;
-// typedef ap_int<16>	data_t;
-// typedef ap_int<24>	acc_t;
-typedef int	coef_t;
-typedef int	data_t;
-typedef int	acc_t;
+typedef ap_int<24>	coef_t;
+typedef ap_int<24>	data_t;
+typedef ap_int<32>	acc_t;
+// typedef int	coef_t;
+// typedef int	data_t;
+// typedef int	acc_t;
 
 
 
