6703b5764a9b6730f1f6ae1d4d0ec73b5e0c3b21
Puze <puzel@student.unimelb.edu.au>
2026-09-06 15:04:21 +1000

    Q5_7

--- _ide/workspace_journal.py
+++ _ide/workspace_journal.py
@@ -115,3 +115,5 @@ comp.run(operation="C_SIMULATION")
 
 comp.run(operation="SYNTHESIS")
 
+comp.run(operation="SYNTHESIS")
+
--- fir128/fir.cpp
+++ fir128/fir.cpp
@@ -67,7 +67,7 @@ void fir (
 
 
     for (int i = 0; i < N; i++) {
-        #pragma HLS unroll factor=2
+        #pragma HLS unroll factor=4
         // #pragma HLS pipeline II=3
         ans += shift_reg[i] * h[i];
     }
